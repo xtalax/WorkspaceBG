119d9d97cfc132efacec8d0886cc337f7397b7ed
xtalax <alex.lemony@gmail.com>
2021-10-13 21:34:22 +0100

    .

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ./bin/* ~/.local/bin/
+cp .workspace_bgs ~
+
+echo "Please edit '~/.workspace_bgs' with paths to your backgrounds, bg1 for Virtual Desktop 1, bg2 for Virtual Desktop 2 etc.\nFinally, add '~/.local/bin/workspacebg' to your login scripts.\nYou'll find this in System Settings>Startup and Shutdown>Autostart"
